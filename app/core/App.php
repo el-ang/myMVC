9b3c2eb893e3dba8dd6e65202629b38797f31552
_roy.exe <elroystmrg@hotmail.com>
2021-10-13 09:32:40 +0800

    routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,23 @@
 <?php
     class App{
+        protected $controller = "";
+        protected $method = "";
+        protected $params = [];
+
         public function __construct(){
-            echo "MVC Ready!";
+            $url = $this->parseURL();
+
+            // Controller
+
+        }
+
+        public function parseURL(){
+            if($_GET["url"]){
+                $url = rtrim($_GET["url"], "/");
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode("/", $url);
+                return $url;
+            }
         }
     }
 ?>
